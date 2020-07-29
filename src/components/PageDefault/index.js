bf8b692c1ef1bcb6234d7ed574511ec9e26eb150
Dario Campagna <campagnacoutinho67@gmail.com>
2020-07-29 19:53:12 -0300

    Melhorando a primeira parte

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from '../Menu';
+import Footer from '../Footer';
+
+function PageDefault(){
+    return (
+        <div>
+            <Menu/>
+        </div>
+    );
+}
+
+export default PageDefault;
---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+
+function CadastroVideo(){
+    return (
+        <PageDefault>Cadastro de Vídeo</PageDefault>
+    );
+}
+
+export default CadastroVideo;
